62905cecf185c94cd02aaf51363e45d1be0033f4
Bagus Muhammad Wijaksono <106671990+baguswijaksono@users.noreply.github.com>
2024-06-30 16:15:29 +0700

    feat: HttpClient

--- crawlcraft.php
+++ crawlcraft.php
@@ -1,1 +1,10 @@
-
+class HttpClient {
+    public function fetch($url) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $output = curl_exec($ch);
+        curl_close($ch);
+        return $output;
+    }
+}
